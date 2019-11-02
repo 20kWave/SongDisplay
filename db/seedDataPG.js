50e31a0496c549701322a1d21f9f90ac1b47c4f6
Brittney Nguyen <brittnguyen@gmail.com>
2019-11-01 20:30:49 -0700

    create seed data songs 1M

--- db/seedDataPG.js
+++ db/seedDataPG.js
@@ -4,8 +4,10 @@ const wave = require('./read20kwave.js');
 
 const writeArists = fs.createWriteStream('artists.csv');
 const writeUsers = fs.createWriteStream('users.csv');
+const writeSongs = fs.createWriteStream('songs.csv');
 writeArists.write('artist_name\n', 'utf8');
 writeUsers.write('name\n', 'utf8');
+writeSongs.write('song_name||upload_time||tag||song_art_url||background_light||background_dark||waveform_data||song_duration||id\n');
 
 function writeArtistUsers(writer, encoding, callback, table) {
 
@@ -15,7 +17,10 @@ function writeArtistUsers(writer, encoding, callback, table) {
     i = 96;
   } else if (table === 'users') {
     i = 2000;
+  } else if (table === 'songs') {
+    i = 960000;
   }
+
   let id = 0;
 
   function write() {
@@ -23,10 +28,17 @@ function writeArtistUsers(writer, encoding, callback, table) {
     do {
       // console.log('wave: ', wave.songs[id]);
       let data = '';
+      if (id === 96) {
+        id = 0;
+      }
+
       if (table === 'artists') {
         data = `${wave.songs[id].artist_name}\n`;
       } else if (table === 'users') {
         data = `${faker.internet.userName()}\n`;
+      } else if (table === 'songs') {
+        var s = wave.songs[id];
+        data = `${s.song_name}||${s.upload_time}|| ${s.tag} ||${s.song_art_url}||${s.background_light} || ${s.background_dark} ||${s.waveform_data} ||${s.song_duration} ||${id}\n`;
       }
 
       i -= 1;
@@ -48,12 +60,16 @@ function writeArtistUsers(writer, encoding, callback, table) {
   write();
 }
 
-writeArtistUsers(writeArists, 'utf-8', () => {
-  writeArists.end();
-}, 'artists');
+// writeArtistUsers(writeArists, 'utf-8', () => {
+//   writeArists.end();
+// }, 'artists');
 
-writeArtistUsers(writeUsers, 'utf-8', () => {
-  writeUsers.end();
-}, 'users');
+// writeArtistUsers(writeUsers, 'utf-8', () => {
+//   writeUsers.end();
+// }, 'users');
+
+writeArtistUsers(writeSongs, 'utf-8', () => {
+  writeSongs.end();
+}, 'songs');
 
 // module.exports.createPGData = createPGData;
